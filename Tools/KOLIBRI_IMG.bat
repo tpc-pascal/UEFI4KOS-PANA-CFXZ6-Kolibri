6d44ac68d1be51543f6bfe12fc2a9bb219ea1dff
tpc-pascal <tpcmemory2005@gmail.com>
2026-04-22 01:06:32 +0700

    Add tools to run img file in VM

---
+++ Tools/KOLIBRI_IMG.bat
@@ -0,0 +1,20 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /B
+)
+
+echo [INFO] Dang khoi chay KolibriOS IMG...
+echo.
+
+set QEMU_PATH="D:\QEMU Iso Boot Test\qemu"
+cd /d %QEMU_PATH%
+
+qemu-system-x86_64.exe -fda KOLIBRI.IMG -m 512 -vga std -net none
+
+if %errorlevel% neq 0 (
+    echo.
+    echo [LOI] QEMU thoat voi ma: %errorlevel%
+    pause
+)
